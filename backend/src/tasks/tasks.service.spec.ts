6ad5bebae7baed03cd15a4e7638b6d215cabd242
Kamran <mkamran1967@gmail.com>
2023-03-06 23:08:55 -0800

    testing mock implementations

--- backend/src/tasks/tasks.service.spec.ts
+++ backend/src/tasks/tasks.service.spec.ts
@@ -1,16 +1,47 @@
-import { Test } from "@nestjs/testing";
-import { TasksService } from "./tasks.service";
-import { TasksRepository } from "./tasks.repository";
+import { Test } from '@nestjs/testing';
+import { TasksService } from './tasks.service';
+import { TasksRepository } from './tasks.repository';
 
-describe('TasksService', () => { 
+// This mock is taking over the TasksRepository
+const mockTasksRepository = () => ({
+  getTasks: jest.fn(),
+  getTaskById: jest.fn(),
+  createTask: jest.fn(),
+  deleteTaskById: jest.fn(),
+  updateTaskStatus: jest.fn(),
+});
+
+const mockUser = {
+  id: 'someid',
+  username: 'test',
+  password: 'test',
+  tasks: [],
+};
+
+describe('TasksService', () => {
   let tasksService: TasksService;
   let tasksRepository: TasksRepository;
 
   beforeEach(async () => {
     // initialize NestJS module with tasksService and tasksRepository
     const module = await Test.createTestingModule({
-      providers: [TasksService],
-    }) 
-  })
+      providers: [
+        TasksService,
+        { provide: TasksRepository, useFactory: mockTasksRepository },
+      ],
+    }).compile(); // must compile
 
- })
+    tasksService = await module.get(TasksService);
+    tasksRepository = await module.get(TasksRepository);
+  });
+
+  // You can nest describe blocks
+  describe('getTasks', () => {
+    it('calls the TasksRepository.getTasks and returns the result', () => {
+      expect(tasksRepository.getTasks).not.toHaveBeenCalled();
+
+      tasksRepository.getTasks(null, mockUser);
+      expect(tasksRepository.getTasks).toHaveBeenCalled();
+    });
+  });
+});
